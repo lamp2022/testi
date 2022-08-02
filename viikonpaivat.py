8f76567e4e72fd9faf845c49c4e40e9a8d406409
lamp2022 <kettunen.t@gmail.com>
2022-08-02 11:08:31 +0300

    fasdfasdfasdfsdf

---
+++ viikonpaivat.py
@@ -0,0 +1,9 @@
+
+when_leave_str = input("When do you leave?")
+what_length_str = input("What lenght?")
+
+when_leave = int(when_leave_str)
+what_length = int(what_length_str)
+
+paluu = (when_leave + what_length) % 7
+print(paluu)
